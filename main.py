0ad49f4fd157ce3e091ae97db6c180f1aa8575ac
Bulat Galimov <ganza.alliance@gmail.com>
2024-09-21 12:51:23 +0300

    new file

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.lesson_1 import file_name
+
+print(file_name("names.txt"))
+
---
+++ src/lesson_1.py
@@ -0,0 +1,18 @@
+def file_name(file_name: str) -> str:
+    with open(f"data/{file_name}", encoding="utf-8") as file:
+        # file.read()
+        format_text = []
+        for line in file.readlines():
+            if line.isalpha():
+                format_text.append(line)
+            else:
+                new_word = ""
+                for lin in line:
+                    if lin.isalpha():
+                        new_word += lin
+                if len(new_word) > 0:
+                    format_text.append(new_word)
+        print(format_text)
+        # return format_text
+    with open("data/{format_file_name.txt}", "w", encoding="utf-8") as new_file:
+        new_file.write("\n".join(format_text))
